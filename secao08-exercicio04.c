c53b23cbded58ddb8d4edad5d78513f376dd1bc3
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-11-19 11:37:46 -0300

    Vetores exercícios

---
+++ secao08-exercicio04.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+    //variavéis
+    int vetor[20], soma = 0;
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("Informe o número para o vetor %d/20: ", (i+1));
+        scanf("%d", &vetor[i]);
+
+        soma = soma + vetor[i];
+
+    }
+    printf("A soma do vetor é %d", soma);    
+}
